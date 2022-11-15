74f41965ec368da83475c430fe38045439149e00
Patana.wo <Patana.wong@hotmail.com>
2022-11-16 01:24:28 +0700

    first commit

---
+++ TestBeryl8/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestBeryl8.Models;
+
+namespace TestBeryl8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Input1(string input1)
+        {
+
+            return Json("200");
+        }
+    }
+}
---
+++ TestBeryl8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestBeryl8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
